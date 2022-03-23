4feed2367510803d16d7155662de53f0812f38a3
nehabht <ne.bhatti2@icloud.com>
2022-03-23 15:53:29 +0100

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
 
 // Chiedi all’utente il suo nome
 
+const user_name = prompt("Come ti chiami?")
+
+// poi chiedi il suo cognome
+
+const user_surname = prompt("Qual è il tuo cognome?")
+
+// poi chiedi il suo colore preferito
+
+const favorite_color = prompt("Qual è il tuo colore preferito?")
+
+// Infine scrivi sulla pagina il risultato in questo formato : nomecognomecolorepreferito22
+
+document.getElementById("your_password").innerHTML=user_name + user_surname + favorite_color + 22
